b52a8ed161d33e52b0bfafd5cf2db8eb43011ad9
Tsito Ny Avo Soa Razafindrakoto <tsitonyavo@gmail.com>
2026-02-08 16:23:23 +0300

    feat: add cart validation schemas for adding, updating, and checkout

---
+++ back-end/src/validations/cart.validation.js
@@ -0,0 +1,46 @@
+import Joi from "joi";
+import { PAYMENT_METHODS } from "../models/StockMovement.js";
+
+const objectId = Joi.string()
+	.pattern(/^[0-9a-fA-F]{24}$/)
+	.message("ID MongoDB invalide");
+
+export const addCartItemSchema = Joi.object({
+	productId: objectId.required().messages({
+		"any.required": "L'identifiant du produit est requis",
+	}),
+	quantity: Joi.number().integer().min(1).required().messages({
+		"any.required": "La quantité est requise",
+		"number.integer": "La quantité doit être un nombre entier",
+		"number.min": "La quantité doit être au minimum 1",
+	}),
+}).strict();
+
+export const updateCartItemSchema = Joi.object({
+	quantity: Joi.number().integer().min(1).required().messages({
+		"any.required": "La quantité est requise",
+		"number.integer": "La quantité doit être un nombre entier",
+		"number.min": "La quantité doit être au minimum 1",
+	}),
+}).strict();
+
+export const checkoutCartSchema = Joi.object({
+	deliveryAddress: Joi.object({
+		street: Joi.string().required().messages({
+			"any.required": "La rue est requise",
+		}),
+		city: Joi.string().required().messages({
+			"any.required": "La ville est requise",
+		}),
+		postalCode: Joi.string().allow(""),
+		country: Joi.string().default("Madagascar"),
+	}).required(),
+	paymentMethod: Joi.string()
+		.valid(...PAYMENT_METHODS)
+		.required()
+		.messages({
+			"any.required": "La méthode de paiement est requise",
+			"any.only": `Méthode de paiement invalide. Valeurs acceptées : ${PAYMENT_METHODS.join(", ")}`,
+		}),
+	notes: Joi.string().max(500).allow(""),
+}).strict();
--- back-end/src/validations/index.js
+++ back-end/src/validations/index.js
@@ -3,3 +3,4 @@ export * from "./user.validation.js";
 export * from "./wallet.validation.js";
 export * from "./shop.validation.js";
 export * from "./stockMovement.validation.js";
+export * from "./cart.validation.js";
